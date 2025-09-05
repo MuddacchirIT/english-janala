a96badad8e31abf92e986e004f532a79434718fd
unknown <protexbeer41@gmail.com>
2025-09-05 16:59:49 +0600

    api connected

--- script/index.js
+++ script/index.js
@@ -3,6 +3,34 @@ const loadLessons = () => {
     .then((res) => res.json()) //promise of json data
     .then((json) => displayLesson(json.data));
 };
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+    console.log(words)
+const wordContainer = document.getElementById("word-container");
+wordContainer.innerHTML = "";
+
+words.forEach((word) => {
+    console.log(word)
+    const card = document.createElement("div");
+    card.innerHTML = `<div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-x-4">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+            <div class="text-2xl font-medium font-bangla">${word.meaning} / ${word.pronunciation}</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>`;
+    wordContainer.append(card);
+});
+};
 
 const displayLesson = (lessons) => {
     // 1. get the container and empty
@@ -12,7 +40,7 @@ const displayLesson = (lessons) => {
     for(let lesson of lessons){
     // 3. create element
     const btnDiv = document.createElement("div");
-    btnDiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
+    btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
     // 4. appent into container
     levelContainer.append(btnDiv);
     }
